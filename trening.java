bcb34179da354c64e54ccf4ed91036e8db3f0719
Poschev38 <poschev38@gmail.com>
2017-03-20 21:42:17 +0800

    Add files via upload

---
+++ trening.java
@@ -0,0 +1,26 @@
+package com.example.tests;
+
+public class trening {
+	public static void main (String args[]){
+		int a = 15;
+		a = count(a);
+			System.out.println(a);
+		}
+	
+	private static int count(int a){
+		for (a = 0; a<10;a++){
+			
+		}
+		return a;
+			
+			
+
+	}	
+}
+
+	
+
+
+	
+
+
